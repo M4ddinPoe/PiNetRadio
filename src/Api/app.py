6d9c903ac617a2cb5d624bf3eb1df2d239081f31
Martin Pöpel <martin.poepel@5minds.de>
2020-09-15 21:54:53 +0200

    add environment

--- src/Api/app.py
+++ src/Api/app.py
@@ -1,16 +1,21 @@
 #!flask/bin/python
 from flask import Flask, jsonify, make_response
+from flask_cors import CORS, cross_origin
 from src.Api.RadioLoader import RadioLoader
 from src.Api.RadiosRpcClient import send_message
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 radios = RadioLoader().load()
 
 @app.route('/')
 def index():
     return "Hello, World!"
 
-@app.route('/radios')
+@app.route('/api/radios')
+@cross_origin()
 def get_radios():
     print(radios)
     response = make_response(
@@ -19,19 +24,21 @@ def get_radios():
     )
     return response
 
-@app.route('/radios/<int:radio_id>/play', methods=['GET'])
+@app.route('/api/radios/<int:radio_id>/play', methods=['GET'])
+@cross_origin()
 def play_radio(radio_id):
 
     radio = radios[radio_id]
     send_message('play', radio['url'])
 
     response = make_response(
-        '',
+        '{}',
         200,
     )
     return response
 
-@app.route('/radios/<int:radio_id>/stop', methods=['GET'])
+@app.route('/api/radios/<int:radio_id>/stop', methods=['GET'])
+@cross_origin()
 def stop_radio(radio_id):
     send_message('stop', '')
 
