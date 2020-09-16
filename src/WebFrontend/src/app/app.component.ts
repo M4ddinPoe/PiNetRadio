3e814e92be7bb457385aaa2ae6a5134315887753
Martin Poepel <martin.poepel@5minds.de>
2020-09-16 17:25:18 +0200

    Add elements; start styling

--- src/WebFrontend/src/app/app.component.ts
+++ src/WebFrontend/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: '<radio-list></radio-list>',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Pi-NetRadio';
